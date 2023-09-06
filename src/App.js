ed77d3373a83716f57a60d1f684aa18a59c508d6
ext.fuwenhao3 <ext.fuwenhao3@jd.com>
2023-09-06 18:27:28 +0800

    add :init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Content from "./Content";
+import Hero from "./Hero";
+import Price from "./Price";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Hero />
+      <Content />
+      <Price />
+      <Footer />
     </div>
   );
 }
